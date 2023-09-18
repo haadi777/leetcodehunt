e14bd6375fb0bef50dc19df1dd0351cf03ac8cf8
Haider Ali <128737704+haadi777@users.noreply.github.com>
2023-09-18 12:11:58 +0500

    Update Problem02.py

--- Problem02.py
+++ Problem02.py
@@ -1,1 +1,9 @@
-
+class Solution:
+     def findMedianSortedArrays(self, nums1, nums2):
+        result=nums1+nums2
+        result.sort()
+        if len(result)%2==0:
+             final=float((result[(int((len(result))/2))]+result[((int((len(result))/2))-1)])/2)
+             return(final)
+        else:
+            return(result[int((len(result))/2)])    
